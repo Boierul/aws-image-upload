1aab1e5322171b172c8762b499c3274f5bf98c45
Dan <pintea.dan988@gmail.com>
2023-02-07 08:59:25 +0100

    Implemented the final api for both upload and download of the image.

--- src/main/java/com/boierul/awsimageupload/filestore/FileStore.java
+++ src/main/java/com/boierul/awsimageupload/filestore/FileStore.java
@@ -3,9 +3,12 @@ package com.boierul.awsimageupload.filestore;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.Optional;
@@ -21,6 +24,7 @@ public class FileStore {
         this.s3 = s3;
     }
 
+    //     Save the file to the S3 bucket
     public void save(String path,
                      String fileName,
                      Optional<Map<String, String>> optionalMetadata,
@@ -38,6 +42,16 @@ public class FileStore {
             s3.putObject(path, fileName, inputStream, metadata);
         } catch (AmazonServiceException e) {
             throw new IllegalStateException("Failed to store file to S3", e);
+        }
+    }
+
+    //     Download the file from the S3 bucket
+    public byte[] download(String path, String key) {
+        try {
+            S3Object object = s3.getObject(path, key);
+            return IOUtils.toByteArray(object.getObjectContent());
+        } catch (AmazonServiceException | IOException e) {
+            throw new IllegalStateException("Failed to download file to s3", e);
         }
     }
 }
